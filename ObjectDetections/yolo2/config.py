e8c412773ce8cc0d66694a0a9871a6afb1154267
Wan Youwen <1756429307@qq.com>
2018-04-20 08:23:22 +0000

    先验框 和 coco类名 配置文件

--- ObjectDetections/yolo2/config.py
+++ ObjectDetections/yolo2/config.py
@@ -1,7 +1,9 @@
+#-*- coding:utf-8 -*-
 """
 Yolov2 anchors and coco classes
 """
 
+# 聚类出来的 5中先验框长宽
 """
 anchors = [[0.738768, 0.874946],
            [2.42204, 2.65704],
@@ -14,12 +16,12 @@ anchors = [[0.57273, 0.677385],
            [3.33843, 5.47434],
            [7.88282, 3.52778],
            [9.77052, 9.16828]]
-
+# coco 数据集 标签类别 名称
 def read_coco_labels():
     f = open("./data/coco_classes.txt")
     class_names = []
-    for l in f.readlines():
-        class_names.append(l[:-1])
+    for l in f.readlines():#每一行
+        class_names.append(l[:-1])#加入到 类名 列表
     return class_names
 
 class_names = read_coco_labels()
